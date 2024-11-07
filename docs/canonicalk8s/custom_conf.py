77d08ebf04f8df495eae38d61a722f110d28a9c3
Nick Veitch <nick.veitch@canonical.com>
2024-11-07 10:45:00 +0000

    really exclude parts

--- docs/canonicalk8s/custom_conf.py
+++ docs/canonicalk8s/custom_conf.py
@@ -185,7 +185,8 @@ custom_required_modules = [
 # Add files or directories that should be excluded from processing.
 custom_excludes = [
     'doc-cheat-sheet*',
-    '_parts/*'
+    '_parts/*',
+    'src/_parts'
     ]
 
 # Add CSS files (located in .sphinx/_static/)
